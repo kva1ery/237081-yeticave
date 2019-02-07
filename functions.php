0fda00c4caed1b35e3c9e3586ccbb8bb52d26a33
Valeriy Kokorev <kokorev.valery@gmail.com>
2019-02-07 17:27:58 +0700

    Разделил главную страницу на шаблоны

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+function currency_format($number) {
+    $number = ceil($number);
+    return number_format($number, 0, ",", " ") . " ₽";
+}
+
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!is_readable($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require $name;
+
+    $result = ob_get_clean();
+
+    return $result;
+}
