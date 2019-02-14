99d2df6c4aa8e7297144e28152e99581af83c8e6
Valeriy Kokorev <kokorev.valery@gmail.com>
2019-02-14 23:27:45 +0700

    Написал скрипт создания базы данных

--- schema.sql
+++ schema.sql
@@ -9,18 +9,40 @@ create table categories (
   name varchar(30) not null
 );
 
+create table users (
+  id            int auto_increment primary key,
+  register_date timestamp not null default current_timestamp,
+  email         varchar(130) not null,
+  name          varchar(100) not null,
+  password      varchar(30) not null,
+  avatar        varchar(50),
+  contacts      varchar(256) not null
+);
+
+create unique index ind_users_email on users(email);
+
 create table lots (
   id          int auto_increment primary key,
   create_date timestamp not null default current_timestamp,
   name        varchar(200) not null,
-  catigory    int not null,
+  category    int not null,
   description text not null,
   image       varchar(50) not null,
   start_price decimal not null,
-  finish_date timestamp not null,
+  finish_date datetime not null,
   price_step  int default 1,
   author      int not null
 );
+
+create index ind_lots_author on lots(author);
+create index ind_lots_category on lots(category);
+create index ind_lots_finish on lots(finish_date);
+create fulltext index ind_lots_fulltext on lots(name, description);
+
+alter table lots
+ add foreign key (category) references categories(id);
+alter table lots
+  add foreign key (author) references users(id);
 
 create table bets (
   id          int auto_increment primary key,
@@ -31,15 +53,12 @@ create table bets (
   win         bit default 0
 );
 
-create table users (
-  id          int auto_increment primary key,
-  register_date timestamp not null default current_timestamp,
-  email       varchar(130) not null,
-  name        varchar(100) not null,
-  password    varchar(30) not null,
-  avatar      varchar(50),
-  contacts    varchar(256) not null
-);
+create index ind_bets_lot on bets(lot);
+create index ind_bets_user on bets(user);
 
+alter table bets
+  add foreign key (lot) references lots(id);
+alter table bets
+  add foreign key (user) references users(id);
 
 -- drop database yeticave;
