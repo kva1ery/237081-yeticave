e2fa5fe930401578dcaf4a8863b64d9f55fbc89b
Valeriy Kokorev <kokorev.valery@gmail.com>
2019-03-03 22:54:53 +0700

    Начал реализацию аутентификации

--- login.php
+++ login.php
@@ -2,20 +2,48 @@
 require_once "functions.php";
 require_once "data.php";
 
-//Заглушка для перехода со страницы регистрации
+
+function validate_form($form) {
+    $required_fields = [
+        "email" => "Введите e-mail",
+        "password"=> "Введите пароль"
+    ];
+    $errors =[];
+    foreach ($required_fields as $field => $error_text) {
+        if (empty($form[$field])) {
+            $errors[$field] = $error_text;
+        }
+    }
+    return $errors;
+}
+
 
 $conn = get_connection();
 $lots_categories = get_categories($conn);
-$user = [];
+$login = [];
 $errors = [];
+$user = [];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $login = $_POST;
+    $errors = validate_form($login);
+
+    if (empty($errors)) {
+        $user = get_user_by_email($conn, $login["email"]);
+    }
+    if (empty($user)) {
+        $errors["email"] = "";
+    }
+}
 
 $page_content = include_template("login.php", [
-
+    "login" => $login,
+    "errors" => $errors
 ]);
 
 $layout_content = include_template("layout.php", [
     "content" => $page_content,
-    "title" => "Регистрация",
+    "title" => "Вход",
     "is_auth" => $is_auth,
     "user_name" => $user_name,
     "lots_categories" => $lots_categories,
