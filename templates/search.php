060001c590d1e5faaa26bede222958001746a113
Kokorev Valeriy <kokorev.va@energokrug.ru>
2019-03-11 10:53:10 +0700

    Поправил шаблон вывода результатов поиска по заданию

--- templates/search.php
+++ templates/search.php
@@ -1,9 +1,13 @@
 <div class="container">
     <section class="lots">
         <h2>Результаты поиска по запросу «<span><?=$search ?? "";?></span>»</h2>
-        <ul class="lots__list">
-            <?=include_template('_lots.php',['lots' => $lots]); ?>
-        </ul>
+            <?php if(!empty($lots)): ?>
+            <ul class="lots__list">
+                <?=include_template('_lots.php',['lots' => $lots]);?>
+            </ul>
+            <?php else: ?>
+                <p>Ничего не найдено по вашему запросу</p>
+            <?php endif; ?>
     </section>
     <?php if (isset($pages)) {
        echo include_template("_pagination.php", ["pages" => $pages]);
